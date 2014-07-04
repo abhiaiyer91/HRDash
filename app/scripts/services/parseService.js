4c27f92ca0de688ffb8ea88d8386dca082334c55
Ryan Hansen <rphansen91@gmail.com>
2014-07-03 19:53:25 -0700

    Branch Off of retrieval integrated into views

--- app/scripts/services/parseService.js
+++ app/scripts/services/parseService.js
@@ -2,6 +2,7 @@ window.app.factory('Parse', function() {
 Parse.initialize("Jh6DNvCXuJwHltaSIovN1ZRd5119mLpckheZtTGA", "UK28hxofsydsjiMpF1ft9dKQjM27ZfJ2PHOoWrDl");
 
 
+
   return {
     get: function(){
       var resumeObj = Parse.Object.extend("Resume");
