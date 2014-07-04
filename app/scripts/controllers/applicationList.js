0ce45dfa59a3d458a7b0af7dc0b0a68cb647cdd6
Ryan Hansen <rphansen91@gmail.com>
2014-07-03 21:26:52 -0700

    App list to App individual view

--- app/scripts/controllers/applicationList.js
+++ app/scripts/controllers/applicationList.js
@@ -1,47 +1,51 @@
 'use strict';
 
-window.app.factory('applications', [function () {
-	var applications = [
-		{name:'Hahakd',job:'111',email:'ryan@anthro.is'},
-		{name:'Jdhdk',job:'222',email:'ryan@anthro.is'},
-		{name:'Kjjc',job:'333',email:'ryan@anthro.is'},
-		{name:'Ykdklc',job:'444',email:'ryan@anthro.is'},
-		{name:'Lkdkd',job:'555',email:'ryan@anthro.is'},
-		{name:'Hdkdll',job:'666',email:'ryan@anthro.is'},
-		{name:'Hahakd',job:'777',email:'ryan@anthro.is'},
-		{name:'Jdhdk',job:'888',email:'ryan@anthro.is'},
-		{name:'Kjjc',job:'999',email:'ryan@anthro.is'},
-		{name:'Ykdklc',job:'111',email:'ryan@anthro.is'},
-		{name:'Lkdkd',job:'222',email:'ryan@anthro.is'},
-		{name:'Hdkdll',job:'333',email:'ryan@anthro.is'},
-		{name:'Hahakd',job:'444',email:'ryan@anthro.is'},
-		{name:'Jdhdk',job:'555',email:'ryan@anthro.is'},
-		{name:'Kjjc',job:'666',email:'ryan@anthro.is'},
-		{name:'Ykdklc',job:'777',email:'ryan@anthro.is'},
-		{name:'Lkdkd',job:'888',email:'ryan@anthro.is'},
-		{name:'Hdkdll',job:'999',email:'ryan@anthro.is'},
-		{name:'Hahakd',job:'111',email:'ryan@anthro.is'},
-		{name:'Jdhdk',job:'222',email:'ryan@anthro.is'},
-		{name:'Kjjc',job:'333',email:'ryan@anthro.is'},
-		{name:'Ykdklc',job:'444',email:'ryan@anthro.is'},
-		{name:'Lkdkd',job:'555',email:'ryan@anthro.is'},
-		{name:'Hdkdll',job:'666',email:'ryan@anthro.is'},
-		{name:'Kyle',job:'777',email:'ryan@anthro.is'}
-	];
-	return function () {
-		return applications;
-	};
-}]);
+// window.app.factory('applications', ['Parse', function (Parse) {
+// 	function successHandler(response){
+// 		console.log('Service');
+// 	    console.log(response);
+// 	    return response;
+// 	}
+// 	function errorHandler(error){
+// 	    console.log(error);
+// 	}
 
-window.app.controller('ApplicationsCtrl', ['$scope', '$location', 'applications', function ($scope, $location, applications) {
-	$scope.applications = applications();
+// 	return function () { 
+// 		return Parse.get().then(successHandler, errorHandler);
+// 	};
+// }]);
+
+window.app.controller('ApplicationsCtrl', ['$scope', '$location', 'Parse', function ($scope, $location, Parse) {
+	function successHandler(response){
+		console.log(response);
+	    $scope.applications = response;
+	    $scope.$apply();
+	}
+	function errorHandler(error){
+	    console.log(error);
+	}
+
+	Parse.get().then(successHandler, errorHandler);
 
 	$scope.view = function (index) {
 		$location.path('/app/' + index);
 	}
 }]);
 
-window.app.controller('ApplicationCtrl', ['$scope', '$route', 'applications', function ($scope, $route, applications) {
-	$scope.app = applications()[$route.current.params.id];
+window.app.controller('ApplicationCtrl', ['$scope', '$route', 'Parse','$sce', function ($scope, $route, Parse, $sce) {
+	function successHandler(response){
+		console.log(response);
+	    $scope.app = response[$route.current.params.id];
+	    $scope.$apply();
+	}
+	function errorHandler(error){
+	    console.log(error);
+	}
+
+	Parse.get().then(successHandler, errorHandler);
+
+	$scope.trustedSrc = function(src){
+    return $sce.trustAsResourceUrl("http://docs.google.com/gview?url=" + src + '&embedded=true&output=embed');
+  }
 
 }]);
